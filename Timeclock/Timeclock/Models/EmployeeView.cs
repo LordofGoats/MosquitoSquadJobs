ef27385bef271418895328db09c6df664efec83f
Carson Beam <spreadthelife@gmail.com>
2015-07-06 22:32:07 -0400

    Upload
    Bootstrap is integrated, _Layout  and Index are formatted, Back-up is
    located on desktop.

---
+++ Timeclock/Timeclock/Models/EmployeeView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Timeclock.Models
+{
+    internal class EmployeeView
+    {
+
+        public string firstName { get; set;}
+        public string lastName { get; set; }
+        public int idNumber { get; set; }
+        public bool active { get; set; }
+    }
+}
