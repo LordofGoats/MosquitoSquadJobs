6af9deca965dba47ad43b330e2f27060cf3cc17d
Carson Beam <spreadthelife@gmail.com>
2015-07-20 09:31:54 -0400

    Created the TurtleShell JS file for future use.

---
+++ TurtleShell/TurtleShell/Eviscerate.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    //var counter = 1
+    $("button.eviscerate").click(function () {
+        $("div.shell").slideToggle();
+        $("div.body").slideToggle()
+        //var counter = 2;
+        //if (counter == 2)
+        //    $("button.eviscerate").click(function () {
+        //        $("div.shell").animate({ right: '300px' });
+        //        var counter = 1;
+            //})
+    })
+});
